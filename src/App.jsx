a362f4b9925ffded4a6e508778b9cfe4e51a3041
Mbonu Chinedum <cboy.chinedu@gmail.com>
2024-07-19 17:30:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// Importing the necessary modules 
+import React, { Component, Fragment } from "react";
+
+
+// Creating the component 
+class App extends Component {
+  // Setting the state 
+  state = {
+
+  }
+
+  // rendering the component 
+  render() {
+    // Returning the component 
+    return (
+      <Fragment> 
+        <p> App component </p>
+      </Fragment>
+    )
+  }
+}
+
+// exporting the componet 
+export default App; 
